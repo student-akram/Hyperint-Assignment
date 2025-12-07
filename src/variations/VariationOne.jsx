eda2d80f1ca3cd651d47f8bb65cac51a4dce1c92
student-akram <akramshaik2004@gmail.com>
2025-12-07 19:10:10 +0530

    Fix case-sensitive file names for Vercel

--- src/variations/VariationOne.jsx
+++ src/variations/VariationOne.jsx
@@ -3,7 +3,7 @@ import React from "react";
 import ProductLayout from "../components/ProductLayout";
 import ReviewSection from "../components/ReviewSection";
 import AnalyticsSection from "../components/AnalyticsSection";
-import SocialProofWidget from "../components/SocialproofWidget";
+import SocialProofWidget from "../components/SocialProofWidget";
 
 const VariationOne = () => {
   return (
